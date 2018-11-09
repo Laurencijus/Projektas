49fb0eb0b5c59760efcb3a9055b389aa63933c70
Laurencijus <39279831+Laurencijus@users.noreply.github.com>
2018-11-09 17:44:56 +0200

    Update config.php

--- config.php
+++ config.php
@@ -1,3 +1,3 @@
 <?php
 const ADMIN_PASSWORD_HASH = '$2y$10$GWO0nCQygkTeXFA8HioCS.zPn/x9Lw6kwPTDDduKN8cLzLdO8heVe';
-const BASE_URL = 'http://localhost/php-10';
+const BASE_URL = 'http://localhost/projektas';
